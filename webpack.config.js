8f5a5249c0684b6262a48d2dab5bddd306423430
kevkrisss <kevin.jonathan5@student.umn.ac.id>
2022-01-31 14:22:51 +0700

    css revision

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       Examples: 'app/components/Examples.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyles: 'app/style/app.css'
+      applicationStyles: 'app/styles/app.css'
     },
     extensions: ['', '.js', '.jsx']
   },
