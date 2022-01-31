88885b772ec0f182f2e32249150a9f9ac43eca37
kevkrisss <kevin.jonathan5@student.umn.ac.id>
2022-01-31 13:53:40 +0700

    add css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About Component</h1>
+      <h1 className="text-center about-page">About Component</h1>
       <p>This is a sentence in React</p>
       <p>Here are some of the tools I used:</p>
       <ul>
