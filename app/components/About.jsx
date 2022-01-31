8e3279ea4cf4c2c73ed1340ab6e3b7e1eb03a220
kevkrisss <kevin.jonathan5@student.umn.ac.id>
2022-01-31 08:59:20 +0700

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,19 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About Component</h1>
+      <p>This is a sentence in React</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> This was the JavaScript framework used
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather  Maps</a> I used this to search for data by city name.
+        </li>
+      </ul>
+    </div>
   )
 };
 
